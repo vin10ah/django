0ca0cf24fffa287961b8db2ac3aad87f65430796
seobin <qkdtjqls@naver.com>
2026-02-04 14:25:04 +0900

    update

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -7,4 +7,6 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='quesiton_create'),
 ]
--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
+from .forms import QuestionForm
 
 
 def index(request):
@@ -15,3 +17,19 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # Answer 모델 직접 사용하는 방식 
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    
+    return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
